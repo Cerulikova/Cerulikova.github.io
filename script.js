8e851ee51ee684bf41501d5d5dcfae3b0e287147
Cerulikova <159918951+Cerulikova@users.noreply.github.com>
2025-02-19 21:24:46 +0100

    rozsirenie stranky

---
+++ script.js
@@ -0,0 +1,30 @@
+class MyHeader extends HTMLElement{
+	connectedCallback(){
+		this.innerHTML =`
+		<header>
+			<h1>Bakalárska práca</h1>
+			<h2>Počítačová hra The Hopper</h2>
+			<nav>
+				<a class="navlink" href="index.html">Domov</a>
+				<a class="navlink" href="diary.html">Denník</a>
+				<a class="navlink" href="work.html">Práca</a>
+				<a class="navlink" href="resources.html">Zdroje</a>
+			</nav>
+		</header>`
+	}	
+}
+
+class MyFooter extends HTMLElement{
+	connectedCallback(){
+		this.innerHTML = `
+		<footer>
+			Vytvorila <strong>Mária Cerulíková</strong> v rámci predmetu Bakalársky seminár.<br>
+			<strong>Mail:</strong> cerulikova2@uniba.sk<br>
+			Obrázok na pozadí od <a href="https://pixabay.com/sk/users/aronvisuals-10427643/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=8433585">Aron Visuals</a> z <a href="https://pixabay.com/sk//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=8433585">Pixabay</a>
+			
+		</footer>`
+	}
+}
+
+customElements.define('my-header', MyHeader)
+customElements.define('my-footer', MyFooter)
